30feb7dc39c253701e0f0adef94edce0b76ad3b2
Sahat Yalkabov <sakhat@gmail.com>
2013-01-02 16:41:25 -0500

    updated middleware

---
+++ config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  db: 'mongodb://localhost/gameon',
+  foursquare: {
+    clientID: "HPAJ2BJUXB11LUWR4OVFWHQTIOIO0DB02XR5MA1KTSGATX0K",
+    clientSecret: "R4QJ2TR2EJVOFGMXTBG0Q415NV3OFR4H0H4YHDS1T54S1QAX",
+    callbackURL: "http://gameon.com/callback"
+  }
+}
