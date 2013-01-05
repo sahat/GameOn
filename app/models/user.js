a36366cc9d4cd7218250ee48a99018d1aa6132d9
Bilal Quadri <bilalquadri92@gmail.com>
2013-01-05 17:37:29 -0500

    Added token generation to users

--- app/models/user.js
+++ app/models/user.js
@@ -7,36 +7,57 @@ var UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, index: { unique: true } },
   password: { type: String, required: true },
+  token: {type: String, required: true},
+  created_on: { type: Date, default: Date.now },
   avatar: String,
   bio: String,
-  token: {type: String, required: true},
-  created_on: { type: Date, default: Date.now }
 });
+
+
+// Function for randomly generating keys
+function randomKey (limit) {
+  limit = (parseInt(limit, 10) > 0 ? parseInt(limit, 10) : 20);
+  var random = '';
+  var list = [ 
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+    'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 
+    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 
+    'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 
+    'Y', 'Z' 
+  ];
+  while (random.length < limit) {
+    random += list[Math.floor(Math.random()*62)];
+  }
+  return random;
+}
 
 
 // Pre save function
 UserSchema.pre('save', function (next) {
   var user = this;
 
-  // hash password only if modified or new
+  // Hash password only if modified or new
   if (!user.isModified('password')) {
     return next();
   }
 
-  // generate a salt
+  // Generate a salt
   bcrypt.genSalt(10, function (err, salt) {
     if (err) {
       return next(err);
     }
 
-    // hash the password with a new salt
+    // Hash the password with a new salt
     bcrypt.hash(user.password, salt, function (err, hash) {
       if (err) {
         return next(err);
       }
 
-      // override text password with the hashed password
+      // Override text password with the hashed password & set token
       user.password = hash;
+      user.token = randomKey();
       next();
     });
   });
