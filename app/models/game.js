065a52084c92c84cc678575f0ce9d83b7c20526a
Bilal Quadri <bilalquadri92@gmail.com>
2013-01-05 11:54:33 -0500

    Updated game model

--- app/models/game.js
+++ app/models/game.js
@@ -7,7 +7,7 @@ var ObjectId = Schema.Types.ObjectId;
 var GameSchema = new Schema({
   sport: String,
   geo: { type: [Number], index: '2d' },
-  players:[ { type: ObjectId, ref: 'User' } ],
+  players:[ {user: { type: ObjectId, ref: 'User' }, joined_on: Date } ],
   description: String,
   created_by: { type: ObjectId, ref: 'User' },
   created_on: { type: Date, default: Date.now },
