1a6ea0343f93c60931ceb05a94ff89802458156e
Bilal Quadri <bilalquadri92@gmail.com>
2013-02-11 15:31:26 -0500

    Updated auth middlewares & added a backdoor

--- config/middlewares/authenticate.js
+++ config/middlewares/authenticate.js
@@ -4,13 +4,15 @@ var User = require('../../app/models/user');
 var mongoose = require('mongoose');
 
 exports.key = function (req, res, next){
+  var hack = req.query.hack;
   var api_key = req.query.api_key;
   var call_id = req.query.call_id;
   var signature = req.query.signature;
 
   var sig = crypto.createHash('md5').update(config.gameon.API_SECRET + call_id).digest("hex");
+  sig = crypto.createHash('md5').update(sig + config.gameon.API_SECRET).digest("hex");
 
-  if (api_key === config.gameon.API_KEY && sig === signature) {
+  if (hack || ( api_key === config.gameon.API_KEY && sig === signature) ) {
     next();
     console.log('AUTHORIZATION SUCCESSFUL');
   } else {
@@ -18,13 +20,13 @@ exports.key = function (req, res, next){
   }
 };
 
-
 exports.user = function(req, res, next){
   var uid = req.body.uid || req.query.uid;
   var token = req.body.token || req.query.token;
   console.log(uid);
   User.findById(mongoose.Types.ObjectId(uid), function (err, user) {
     if (!err && user) {
+      // TODO: Make sure token validates
       console.log('200: AUTHENTICATION SUCCESSFUL');
       next();
     } else {
