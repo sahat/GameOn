591ddf80cc71470cd32525283a831dcee6a50027
Bilal Quadri <bilalquadri92@gmail.com>
2013-05-28 03:12:18 -0400

    Cleaner middlewares in routes

--- config/routes.js
+++ config/routes.js
@@ -2,30 +2,40 @@
 var users = require('../app/controllers/users'),
     games = require('../app/controllers/games'),
     comments = require('../app/controllers/comments'),
-    auth = require('./middleware/authenticate');
+    auth = require('./middleware/authenticate'),
+    validate_client = auth.key,
+    require_authentication = auth.user;
 
 module.exports = function(app) {
 
+  // All requests must come from a valid client
+  app.all('*', validate_client);
+
+  // User registration & login
+  app.post('/register', users.register);
+  app.post('/login', users.login);
+
+  // All other requests require authentication
+  app.all('*', require_authentication);
+
   // Users
-  app.post('/register', auth.key, users.register);
-  app.post('/login', auth.key, users.login);
-  app.get('/users', auth.key, auth.user, users.get_all);
-  app.get('/users/:id', auth.key, auth.user, users.get_user);
+  app.get('/users', users.get_all);
+  app.get('/users/:id', users.get_user);
 
   // Games
-  app.get('/games', auth.key, auth.user, games.get_all_games);
-  app.post('/games', auth.key, auth.user, games.create);
-  app.get('/games/:game_id', auth.key, games.get_game);
-  app.put('/games/:game_id', auth.key, auth.user, games.edit);
-  app.del('/games/:game_id', auth.key, auth.user, games.delete);
-  app.get('/games/user/:user_id', auth.key, auth.user, games.user);
-  app.get('/games/nearby', auth.key, auth.user, games.nearby);
-  app.post('/games/join', auth.key, auth.user, games.join);
-  app.post('/games/leave', auth.key, auth.user, games.join);
+  app.get('/games', games.get_all_games);
+  app.post('/games', games.create);
+  app.get('/games/:game_id', games.get_game);
+  app.put('/games/:game_id', games.edit);
+  app.del('/games/:game_id', games.delete);
+  app.get('/games/user/:user_id', games.user);
+  app.get('/games/nearby', games.nearby);
+  app.post('/games/join', games.join);
+  app.post('/games/leave', games.join);
 
   // Comments
-  app.post('/comments/:game_id', auth.key, auth.user, comments.create_comment);
-  app.del('/comments/:game_id', auth.key, auth.user, comments.delete_comment);
-  app.get('/comments/:game_id', auth.key, auth.user, comments.get_comments);
+  app.post('/comments/:game_id', comments.create_comment);
+  app.del('/comments/:game_id', comments.delete_comment);
+  app.get('/comments/:game_id', comments.get_comments);
 
 };
